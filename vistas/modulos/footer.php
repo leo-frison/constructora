64691f73b76a7c2c6d3018dbf8b3f20a90aa2c1c
Leonardo Frison <leo.sistemas@outlook.com>
2021-01-15 09:58:56 -0300

    Subiendo Codigode un desarrollo viejo en php

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2021 <a href="https://leonardofrison.com" target="_blank">MiSitio</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
---
+++ vistas/modulos/usuarios.html.php
@@ -0,0 +1,134 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Administrar usuarios
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Administrar usuarios</li>
+    
+    </ol>
+
+  </section>
+
+  <section class="content">
+
+    <div class="box">
+
+      
+      <div class="box-body">
+        
+       <table class="table table-bordered table-striped dt-responsive tablas">
+         
+        <thead>
+         
+         <tr>
+           
+           <th style="width:10px">#</th>
+           <th>Nombre</th>
+           <th>Usuario</th>
+           <th>Foto</th>
+           <th>Perfil</th>
+           <th>Estado</th>
+           <th>Último login</th>
+           <th>Acciones</th>
+
+         </tr> 
+
+        </thead>
+
+        <tbody>
+          
+          <tr>
+            <td>1</td>
+            <td>Usuario Administrador</td>
+            <td>admin</td>
+            <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-success btn-xs">Activado</button></td>
+            <td>2017-12-11 12:05:32</td>
+            <td>
+
+              <div class="btn-group">
+                  
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+              </div>  
+
+            </td>
+
+          </tr>
+
+           <tr>
+            <td>1</td>
+            <td>Usuario Administrador</td>
+            <td>admin</td>
+            <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-success btn-xs">Activado</button></td>
+            <td>2017-12-11 12:05:32</td>
+            <td>
+
+              <div class="btn-group">
+                  
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+              </div>  
+
+            </td>
+
+          </tr>
+
+           <tr>
+            <td>1</td>
+            <td>Usuario Administrador</td>
+            <td>admin</td>
+            <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
+            <td>2017-12-11 12:05:32</td>
+            <td>
+
+              <div class="btn-group">
+                  
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+              </div>  
+
+            </td>
+
+          </tr>
+
+        </tbody>
+
+       </table>
+
+      </div>
+
+    </div>
+
+  </section>
+
+</div>
+
+
+
+      
+
+
+</div>
+
+
